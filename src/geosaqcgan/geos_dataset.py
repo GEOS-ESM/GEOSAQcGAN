b92411205a7aea28124216a68966dce690e5fce6
Patricia Castellanos <patricia.castellanos@nasa.gov>
2025-05-13 13:49:35 -0400

    update timespan to be equal to n_timesteps

--- src/geosaqcgan/geos_dataset.py
+++ src/geosaqcgan/geos_dataset.py
@@ -138,7 +138,7 @@ class GEOSCFLargeDataset(Dataset):
         """
         Returns the number of unique input output time sequences
         """
-        return self.n_timesteps - self.window_size
+        return self.n_timesteps 
 
     @property
     def n_levels(self) -> int:
